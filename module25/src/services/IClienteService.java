62e52460d564d716a9a5410f75167e4aa773a855
williannlemoss <willianhophip@gmail.com>
2024-10-21 11:14:40 -0300

    module25

---
+++ module25/src/services/IClienteService.java
@@ -0,0 +1,16 @@
+package services;
+
+import domain.Cliente;
+import exception.TipoChaveNaoEncontradaException;
+
+public interface IClienteService {
+
+	Boolean cadastrar(Cliente cliente) throws TipoChaveNaoEncontradaException;
+
+	Cliente buscarPorCPF(Long cpf);
+
+	void excluir(Long cpf);
+
+	void alterar(Cliente cliente) throws TipoChaveNaoEncontradaException;
+
+}
